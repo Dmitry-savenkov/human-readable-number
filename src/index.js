220613f970e248ec8c5fa2152aa39005b7c9085a
Dmitry_Savenkov <73306821+Dmitry-savenkov@users.noreply.github.com>
2021-02-28 16:10:43 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
 module.exports = function toReadable(number) {
-  
-  const onesText = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nin", "ten", "eleven", "twelve", 'thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
-  const tensText = ["", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "];
+  const NumberOne = ['',
+                    'one',
+                    'two',
+                    'three',
+                    'four',
+                    'five',
+                    'six',
+                    'seven',
+                    'eight',
+                    'nine', 
+                    'ten',
+                    'eleven',
+                    'twelve',
+                    'thirteen',
+                    'fourteen',
+                    'fifteen',
+                    'sixteen',
+                    'seventeen',
+                    'eighteen',
+                    'nineteen'];
+  const NumberTen = ['',
+                    '',
+                    'twenty ',
+                    'thirty ',
+                    'forty ',
+                    'fifty ',
+                    'sixty ',
+                    'seventy ',
+                    'eighty ',
+                    'ninety '];
 
-  if (number == 0) {
-      return 'zero';
-  }
 
-  const hundreds = Math.floor( number / 100 );
-
+ if (number === 0){
+     return 'zero';
+ }
+  const hundreds = Math.floor(number / 100);
   let ten = Math.floor((number - hundreds * 100) / 10);
-  let one = number - (hundreds * 100) - (tens * 10);
-
-  if (ten == 1) {
+  let one = number - hundreds * 100 - tens * 10;
+  if (ten === 1) {
+    one = number - hundreds * 100;
     ten = 0;
-    one = number - (hundreds * 100);
   }
-
-  const hundreds = hundreds ? onesText[hundreds] + ' hundred ' : '';
-  return (hundreds + tensText[ten] + onesText[one]).trim()
-};
+  const hundredsText = hundreds ? NumberOne[hundreds] + ' hundred ' : '';
+  return (hundredsText + NumberTen[ten] + NumberOne[one]).trim()
+}
