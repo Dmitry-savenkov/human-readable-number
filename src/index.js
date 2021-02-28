b29d28a6791e0658843b2dc76aca681d39675405
Dmitry_Savenkov <73306821+Dmitry-savenkov@users.noreply.github.com>
2021-02-28 15:57:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
   
-}
+  const onesText = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nin", "ten", "eleven", "twelve", 'thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  const tensText = ["", "", "twenty ", "thirty ", "forty ", "fifty ", "sixty ", "seventy ", "eighty ", "ninety "];
+
+  if (number == 0) {
+      return 'zero';
+  }
+
+  const hundreds = Math.floor( number / 100 );
+
+  let ten = Math.floor((number - hundreds * 100) / 10);
+  let one = number - (hundreds * 100) - (tens * 10);
+
+  if (ten == 1) {
+    ten = 0;
+    one = number - (hundreds * 100);
+  }
+
+  const hundreds = hundreds ? onesText[hundreds] + ' hundred ' : '';
+  return (hundreds + tensText[ten] + onesText[one]).trim()
+};
