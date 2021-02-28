28063301ae9609ff23c4a0f860744444ecc418de
Dmitry_Savenkov <73306821+Dmitry-savenkov@users.noreply.github.com>
2021-02-28 16:16:08 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,13 @@
 module.exports = function toReadable(number) {
-  const NumberOne = ['',
-                    'one',
-                    'two',
-                    'three',
-                    'four',
-                    'five',
-                    'six',
-                    'seven',
-                    'eight',
-                    'nine', 
-                    'ten',
-                    'eleven',
-                    'twelve',
-                    'thirteen',
-                    'fourteen',
-                    'fifteen',
-                    'sixteen',
-                    'seventeen',
-                    'eighteen',
-                    'nineteen'];
-  const NumberTen = ['',
-                    '',
-                    'twenty ',
-                    'thirty ',
-                    'forty ',
-                    'fifty ',
-                    'sixty ',
-                    'seventy ',
-                    'eighty ',
-                    'ninety '];
-
+  const  NumberOne = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const  NumberTen = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
 
  if (number === 0){
      return 'zero';
  }
   const hundreds = Math.floor(number / 100);
   let ten = Math.floor((number - hundreds * 100) / 10);
-  let one = number - hundreds * 100 - tens * 10;
+  let one = number - hundreds * 100 - ten * 10;
   if (ten === 1) {
     one = number - hundreds * 100;
     ten = 0;
